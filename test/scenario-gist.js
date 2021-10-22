c1c405c842f79795d6ec1cb518eb77177055ff8b
Ilya Afanasenko <fg1@mail.ru>
2021-10-22 15:33:18 +0300

    test: added third user

--- test/scenario-gist.js
+++ test/scenario-gist.js
@@ -3,14 +3,16 @@ const { Scenario } = require('..');
 module.exports = async () => {
     const scenario = new Scenario('An example of synchronizing multiple tests');
 
-    const [user1, user2] = await Promise.all([
+    const [user1, user2, user3] = await Promise.all([
         scenario.createUser('First user', 'test/first-user-test.js', 'chrome'),
-        scenario.createUser('Second user', 'test/second-user-test.js', 'chrome --incognito')
+        scenario.createUser('Second user', 'test/second-user-test.js', 'chrome --incognito'),
+        scenario.createUser('Third user', 'test/third-user-test.js', 'chrome --incognito')
     ]);
 
     await Promise.all([
         user1.runStage('Check page load'),
-        user2.runStage('Check page load')
+        user2.runStage('Check page load'),
+        user3.runStage('Check page load')
     ]);
 
     await user1.runStage('Type a color and send it');
@@ -19,6 +21,10 @@ module.exports = async () => {
     await user2.runStage('Type a color and send it');
     await user2.runStage('Check result');
 
+    await user3.runStage('Type a color and send it');
+    await user3.runStage('Check result');
+
     user1.runStage('End');
     user2.runStage('End');
+    user3.runStage('End');
 };
